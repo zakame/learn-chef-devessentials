6ebd9524dd336af58e9d1be0de3059c6eafe7aff
Zak B. Elep <zakame@zakame.net>
2018-05-03 13:25:08 +0800

    adds apache recipe to recipes/default.rb

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,16 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+#
+# This is NOT my first attempt to edit using Vim.
+# And here is another line, using Emacs.
+
+package 'httpd'
+
+file '/var/www/html/index.html' do
+  content '<h1>Hello, world!</h1>'
+end
+
+service 'httpd' do
+  action [:enable, :start]
+end
